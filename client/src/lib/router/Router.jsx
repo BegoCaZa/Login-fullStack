d23a7da6b26569e60edb90418e590bacd312a238
BegoCaZa <begoxcz@hotmail.com>
2025-06-19 17:20:58 +0200

    update

--- client/src/lib/router/Router.jsx
+++ client/src/lib/router/Router.jsx
@@ -1,12 +1,16 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from '../../pages/home/Home';
 import Profile from '../../pages/profile/Profile';
+import LoginPage from '../../pages/login/LoginPage';
+import RegisterPage from '../../pages/register/RegisterPage';
 
 const Router = () => {
 	return (
 		<>
 			<Routes>
 				<Route path='/' element={<Home />} />
+				<Route path='/login' element={<LoginPage />} />
+				<Route path='/register' element={<RegisterPage />} />
 				<Route path='/profile' element={<Profile />} />
 			</Routes>
 		</>
